ab28e2c0bd63bd852993a811255d891380e55585
justin reid <hi@justinre.id>
2022-08-19 14:41:08 +0100

    models created

---
+++ models/player.py
@@ -0,0 +1,6 @@
+class Player:
+    def __init__(self, name, position, fatigue, id = None):
+        self.name = name
+        self.position = position
+        self.fatigue = fatigue
+        self.id = id
---
+++ models/training.py
@@ -0,0 +1,5 @@
+class Training:
+    def __init__(self, time, duration, id = None):
+        self.time = time
+        self.duration = duration
+        self.id = id
