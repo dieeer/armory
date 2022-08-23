faab318ae597a380723dc7fe13e7eb4ccf9514f6
justin reid <hi@justinre.id>
2022-08-23 12:08:14 +0100

    added new table to database for current squad

--- console.py
+++ console.py
@@ -3,6 +3,7 @@ from controllers.player_training_controller import player_training
 from models.player import Player
 from models.training import Training
 from models.player_training import Player_Training
+from models.current_squad import CurrentSquad
 
 import repositories.player_repository as player_repository
 import repositories.training_repository as training_repository
@@ -14,13 +15,10 @@ player_training_repository.delete_all()
 
 player1 = Player('Bukayo Saka', 7, 'RW', 1)
 player_repository.save(player1)
-
 player2 = Player('Emile Smith-Rowe', 10, 'CAM', 10)
 player_repository.save(player2)
-
 player3 = Player('Kieran Tierney', 2, 'LWB', 60)
 player_repository.save(player3)
-
 player4 = Player('Aaron Ramsdale', 1, 'GK', 0)
 player_repository.save(player4)
 
--- db/football.sql
+++ db/football.sql
@@ -24,3 +24,24 @@ CREATE TABLE players_trainings (
     training_id INT NOT NULL REFERENCES trainings(id) ON DELETE CASCADE,
     comments TEXT
 );
+
+CREATE TABLE current_squad (
+    id SERIAL PRIMARY KEY,
+    gk_id INT REFERENCES players(id),
+    lb_id INT REFERENCES players(id),
+    cb1_id INT REFERENCES players(id),
+    cb2_id INT REFERENCES players(id),
+    rb_id INT REFERENCES players(id),
+    lm_id INT REFERENCES players(id),
+    cm1_id INT REFERENCES players(id),
+    cm2_id INT REFERENCES players(id),
+    rm_id INT REFERENCES players(id),
+    cam_id INT REFERENCES players(id),
+    st_id INT REFERENCES players(id),
+    sub1_id INT REFERENCES players(id),
+    sub2_id INT REFERENCES players(id),
+    sub3_id INT REFERENCES players(id),
+    sub4_id INT REFERENCES players(id),
+    sub5_id INT REFERENCES players(id),
+    sub6_id INT REFERENCES players(id)
+);
